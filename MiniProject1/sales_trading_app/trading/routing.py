c60a7083751474d8a182503ae7521f7953e09770
Zhalgas Bagytzhan <zhalgasbagytzhan@Noutbuk-Zhalgas-2.local>
2025-02-27 22:16:32 +0500

    MINI

--- MiniProject1/sales_trading_app/trading/routing.py
+++ MiniProject1/sales_trading_app/trading/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .consumers import OrderBookConsumer
+from .consumers import OrderBookConsumer, PriceChartConsumer
 
 websocket_urlpatterns = [
     path('ws/trading/orders/', OrderBookConsumer.as_asgi()),
+    path('ws/trading/prices/', PriceChartConsumer.as_asgi()),
 ]
